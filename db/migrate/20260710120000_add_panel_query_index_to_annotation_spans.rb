f831ef0bf413808f9c8e7a053c869737cafe8d4c
Andrei Makarov <andrei@kiskolabs.com>
2026-07-10 14:35:55 +0300

    Add migration to create an index on annotation_spans for improved query performance

---
+++ db/migrate/20260710120000_add_panel_query_index_to_annotation_spans.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AddPanelQueryIndexToAnnotationSpans < ActiveRecord::Migration[8.1]
+  def change
+    add_index :annotation_spans,
+      %i[annotation_review_id field_name content_revision_version],
+      name: "index_annotation_spans_on_review_field_and_revision",
+      if_not_exists: true
+  end
+end
